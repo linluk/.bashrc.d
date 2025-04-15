3a53f891fa8791cb28de88d152eaa5560e7d7f5a
Lukas Singer <lukas42singer@gmail.com>
2025-04-15 23:58:51 +0200

    man with colors

--- 20-functions.sh
+++ 20-functions.sh
@@ -5,3 +5,16 @@ function _mkdir_cd() {
     mkdir -p "$1" && cd "$1" ;
 }
 
+# aliased to man in ~/.bashrc.d/[**]-aliases.sh
+function _man_with_colors() {
+    # https://www.geeksforgeeks.org/how-to-view-colored-man-pages-in-linux/
+    export LESS_TERMCAP_mb=$'\e[01;31m'       # begin blinking
+    export LESS_TERMCAP_md=$'\e[01;37m'       # begin bold
+    export LESS_TERMCAP_me=$'\e[0m'           # end all mode like so, us, mb, md, mr
+    export LESS_TERMCAP_se=$'\e[0m'           # end standout-mode
+    export LESS_TERMCAP_so=$'\e[45;93m'       # start standout mode
+    export LESS_TERMCAP_ue=$'\e[0m'           # end underline
+    export LESS_TERMCAP_us=$'\e[4;93m'        # start underlining
+
+    command man "$@"
+}
